a174817adb881df5272f11b80004706345f51cf4
Dean Fletcher <maxfletch101@yahoo.com>
2023-01-31 11:30:32 -0500

    left, right, up, down arrow logic

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const c = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
+const gravity = 0.9
+
 class Player {
     constructor() {
         this.position = {
@@ -26,16 +28,70 @@ class Player {
 
     update() {
         this.draw()
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
+
+        // Fall until bottom of ground
+        if (this.position.y + this.height + this.velocity.y <= canvas.height)
+            this.velocity.y += gravity  // accelerating over time
+        else this.velocity.y = 0
     }
 }
 
 const player = new Player()
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    }
+}
 
 function animate() {
     requestAnimationFrame(animate)                  // recursively call
     c.clearRect(0, 0, canvas.width, canvas.height)  // clear canvas, to maintain players shape
     player.update()
+
+    if (keys.right.pressed) {
+        player.velocity.x = 5
+    } else if (keys.left.pressed) {
+        player.velocity.x = -5
+    } else player.velocity.x = 0
 }
 
 animate()
+
+// listener when key pressed 
+addEventListener('keydown', ({ code: key }) => {
+    switch (key) {
+        case 'ArrowUp':
+            player.velocity.y -= 10
+            break
+        case 'ArrowDown':
+            break
+        case 'ArrowLeft':
+            keys.left.pressed = true
+            break
+        case 'ArrowRight':
+            keys.right.pressed = true
+            break
+    }
+})
+
+// listener when key not pressed
+addEventListener('keyup', ({ code: key }) => {
+    switch (key) {
+        case 'ArrowUp':
+            player.velocity.y -= 10
+            break
+        case 'ArrowDown':
+            break
+        case 'ArrowLeft':
+            keys.left.pressed = false
+            break
+        case 'ArrowRight':
+            keys.right.pressed = false
+            break
+    }
+})
